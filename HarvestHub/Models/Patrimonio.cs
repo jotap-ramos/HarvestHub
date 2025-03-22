9a7ecd2b9cb794dc5e1fc91b3c51de51e1843ea2
João Ramos <jppramoss@gmail.com>
2025-03-22 18:07:50 -0300

    feat: add model class for Patrimonio

---
+++ HarvestHub/Models/Patrimonio.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HarvestHub.Models
+{
+    public class Patrimonio
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string? Nome { get; set; }
+        [Required]
+        public string? Categoria { get; set; }
+        [Required]
+        public decimal? Valor { get; set; }
+
+    }
+}
