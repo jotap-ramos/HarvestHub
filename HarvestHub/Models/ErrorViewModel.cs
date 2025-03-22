fbdfb76ffb641a1f3fa0972079129e1cab338ab0
João Ramos <jppramoss@gmail.com>
2025-03-22 17:48:17 -0300

    chore: initialize project structure

---
+++ HarvestHub/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HarvestHub.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
