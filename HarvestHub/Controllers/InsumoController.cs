593090cc2297985ef7cae078627c9c52fae9cc50
joaoCarlosDev <joaocarlosprocarvalho@gmail.com>
2025-03-30 06:07:12 -0300

    noovo commit, removendo coisas

--- HarvestHub/Controllers/InsumoController.cs
+++ HarvestHub/Controllers/InsumoController.cs
@@ -18,31 +18,13 @@ namespace HarvestHub.Controllers
         {
             _context = context;
         }
-
+        
         // GET: Insumo
         public async Task<IActionResult> Index()
         {
             return View(await _context.Insumos.ToListAsync());
         }
-
-        // GET: Insumo/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var insumo = await _context.Insumos
-                .FirstOrDefaultAsync(m => m.IdInsumo == id);
-            if (insumo == null)
-            {
-                return NotFound();
-            }
-
-            return View(insumo);
-        }
-
+        
         // GET: Insumo/Create
         public IActionResult Create()
         {
@@ -50,8 +32,6 @@ namespace HarvestHub.Controllers
         }
 
         // POST: Insumo/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("IdInsumo,TipoInsumo,CodInsumo,Volume,Custo,Descricao,Marca,GerenteDeProducaoCrea")] Insumo insumo)
@@ -64,57 +44,7 @@ namespace HarvestHub.Controllers
             }
             return View(insumo);
         }
-
-        // GET: Insumo/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var insumo = await _context.Insumos.FindAsync(id);
-            if (insumo == null)
-            {
-                return NotFound();
-            }
-            return View(insumo);
-        }
-
-        // POST: Insumo/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("IdInsumo,TipoInsumo,CodInsumo,Volume,Custo,Descricao,Marca,GerenteDeProducaoCrea")] Insumo insumo)
-        {
-            if (id != insumo.IdInsumo)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(insumo);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!InsumoExists(insumo.IdInsumo))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(insumo);
-        }
+        
 
         // GET: Insumo/Delete/5
         public async Task<IActionResult> Delete(int? id)
@@ -148,10 +78,6 @@ namespace HarvestHub.Controllers
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
-
-        private bool InsumoExists(int id)
-        {
-            return _context.Insumos.Any(e => e.IdInsumo == id);
-        }
+        
     }
 }
--- HarvestHub/Controllers/ProducaoController.cs
+++ HarvestHub/Controllers/ProducaoController.cs
@@ -18,31 +18,10 @@ namespace HarvestHub.Controllers
         {
             _context = context;
         }
-
-        // GET: Producao
         public async Task<IActionResult> Index()
         {
             return View(await _context.Producoes.ToListAsync());
         }
-
-        // GET: Producao/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var producao = await _context.Producoes
-                .FirstOrDefaultAsync(m => m.IdProducao == id);
-            if (producao == null)
-            {
-                return NotFound();
-            }
-
-            return View(producao);
-        }
-
         // GET: Producao/Create
         public IActionResult Create()
         {
@@ -50,8 +29,6 @@ namespace HarvestHub.Controllers
         }
 
         // POST: Producao/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("IdProducao,Tipo,Custo,Volume")] Producao producao)
@@ -60,98 +37,10 @@ namespace HarvestHub.Controllers
             {
                 _context.Add(producao);
                 await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                return RedirectToAction(nameof(Create));
             }
             return View(producao);
         }
 
-        // GET: Producao/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var producao = await _context.Producoes.FindAsync(id);
-            if (producao == null)
-            {
-                return NotFound();
-            }
-            return View(producao);
-        }
-
-        // POST: Producao/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("IdProducao,Tipo,Custo,Volume")] Producao producao)
-        {
-            if (id != producao.IdProducao)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(producao);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!ProducaoExists(producao.IdProducao))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(producao);
-        }
-
-        // GET: Producao/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var producao = await _context.Producoes
-                .FirstOrDefaultAsync(m => m.IdProducao == id);
-            if (producao == null)
-            {
-                return NotFound();
-            }
-
-            return View(producao);
-        }
-
-        // POST: Producao/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var producao = await _context.Producoes.FindAsync(id);
-            if (producao != null)
-            {
-                _context.Producoes.Remove(producao);
-            }
-
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
-        private bool ProducaoExists(int id)
-        {
-            return _context.Producoes.Any(e => e.IdProducao == id);
-        }
     }
 }
