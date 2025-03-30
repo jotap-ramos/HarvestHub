b1bd6ad163f40c52888c7c6dc251d5f38f6b4726
cubidev <cubidev.vitu@gmail.com>
2025-03-29 21:09:17 -0300

    Recursos Humanos e Gerente de produção funcionais

---
+++ HarvestHub/ViewModels/GerenteDeProducaoViewModel.cs
@@ -0,0 +1,32 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HarvestHub.ViewModels
+{
+    public class GerenteDeProducaoViewModel
+    {
+        [Key]
+        public int FuncionarioId { get; set; }
+        [Required, MaxLength(45)]
+        public required string Nome { get; set; }
+
+        [Required, DataType(DataType.Currency), Column(TypeName = "decimal(10, 2)")]
+        public decimal Salario { get; set; }
+
+        [Required, DataType(DataType.Date)]
+        public DateTime DataAdmissao { get; set; }
+
+        [Required, MaxLength(11)]
+        public required string CPF { get; set; }
+
+        [Required, DataType(DataType.Date)]
+        public DateTime DataNascimento { get; set; }
+
+        [Required]
+        public short Status { get; set; }
+
+        [Required, MaxLength(9)]
+        public required string CREA { get; set; }
+    }
+}
