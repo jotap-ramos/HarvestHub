306598feba55eb4de42bb62af058b024a7c60a86
cubidev <cubidev.vitu@gmail.com>
2025-03-29 21:09:17 -0300

    Recursos Humanos e Gerente de produção funcionais

--- HarvestHub/Data/ApplicationContext.cs
+++ HarvestHub/Data/ApplicationContext.cs
@@ -6,4 +6,6 @@ public class ApplicationContext(DbContextOptions<ApplicationContext> options) : 
 {
     public DbSet<HarvestHub.Models.Contador> Contador { get; set; } = default!;
     public DbSet<HarvestHub.Models.Funcionario> Funcionario { get; set; } = default!;
+    public DbSet<HarvestHub.Models.GerenteDeProducao> GerenteDeProducao { get; set; } = default!;
+    public DbSet<HarvestHub.Models.RecursosHumanos> RecursosHumanos { get; set; } = default!;
 }
