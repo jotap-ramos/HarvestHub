7abea8287c2bbf891ce470ae8a0f03695053ca61
João Ramos <jppramoss@gmail.com>
2025-03-30 02:02:15 -0300

    chore: adiciona classe model de contrato

---
+++ HarvestHub/Models/Contrato.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HarvestHub.Models
+{
+    public class Contrato
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(11)]
+        public string? NumeroContrato { get; set; }
+        public DateTime DataInicio { get; set; }
+        public DateTime DataFim { get; set; }
+        public decimal ValorTotal { get; set; }
+        public int FornecedorId { get; set; }
+        public Fornecedor? Fornecedor { get; set; }
+    }
+}
