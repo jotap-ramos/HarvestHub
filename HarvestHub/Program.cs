b61c933d1200e3af3a22867ffecf1241b3352cc1
cubidev <cubidev.vitu@gmail.com>
2025-03-30 19:42:44 -0300

    usar postgres

--- HarvestHub/Program.cs
+++ HarvestHub/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<HarvestHubContext>(options =>
-options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString")));
+options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnectionString")));
 
 var app = builder.Build();
 
