64342565d8834f3d2002855d8ecdd5ca7b0dc87d
João Ramos <jppramoss@gmail.com>
2025-03-30 02:01:15 -0300

    chore: adiciona classe model do fornecedor

---
+++ HarvestHub/Models/Fornecedor.cs
@@ -0,0 +1,11 @@
+﻿namespace HarvestHub.Models;
+
+public class Fornecedor
+{
+    public int Id { get; set; }
+    public string? Nome { get; set; }
+    public string? CNPJ { get; set; }
+    public string? Telefone { get; set; }
+    public string? Email { get; set; }
+    public List<Contrato>? Contratos { get; set; }
+}
